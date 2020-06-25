74ae6d2ceb0abe69266be82f6b51bd134d66ece5
Sam Morrison <sorrison@gmail.com>
2020-06-26 08:39:15 +1000

    Make andy.sh run on all quarantined files

--- andy.sh
+++ andy.sh
@@ -26,20 +26,18 @@ fi
 echo "Testing quarantine files..."
 for FILE in $(cat $QUARANTINE); do
     FPATH=$(echo $FILE | awk -F'/' '{print $(NF-1)"/"$NF}')
-    for F in $(grep $FPATH $OBJECTS); do
-        echo "Checking $FPATH"
-        curls=$(swift-object-info $F | grep '^curl' | grep -v Handoff)
-        count=0
-        for curlcmd in $curls; do
-            http_code=$(eval "$curlcmd --path-as-is -s -o /dev/null -w '%{http_code}'")
-            [[ $http_code -eq 200 ]] && count=$((count+1))
-        done
-        echo "Found $count copies of $FPATH"
-        if [[ $count -ge 2 ]]; then
-            rm -v $FILE
-        else
-            echo "Running jake.sh for $FILE"
-            ./jake.sh $FILE
-        fi
+    echo "Checking $FPATH"
+    curls=$(swift-object-info $FILE | grep '^curl' | grep -v Handoff)
+    count=0
+    for curlcmd in $curls; do
+        http_code=$(eval "$curlcmd --path-as-is -s -o /dev/null -w '%{http_code}'")
+        [[ $http_code -eq 200 ]] && count=$((count+1))
     done
+    echo "Found $count copies of $FPATH"
+    if [[ $count -ge 2 ]]; then
+        rm -v $FILE
+    else
+        echo "Running jake.sh for $FILE"
+        ./jake.sh $FILE
+    fi
 done
