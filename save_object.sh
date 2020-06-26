ac6732967c1f68d968935fbd4299406ea90e6cd7
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 15:52:03 +1000

    read hashes from URL and do things

--- save_object.sh
+++ save_object.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # save_object.sh
 
-hash=$1
+URL="https://swift.rc.nectar.org.au/v1/AUTH_f42f9588576c43969760d81384b83b1f/public/missing_object_hash"
 
-qloc=`grep ${hash} /root/swift-quarantine-*.txt`
+for hash in $(curl -s $URL); do
+  echo "Looking for ${hash}"
 
-if [[ ! -z ${qloc} ]]; then
-  echo "jake is attempting to save object ${hash}"
-  ./jake.sh ${qloc}
-fi
+  for qfile in $(ls /root/swift-quarantine-*.txt); do
+    qloc=`grep ${hash} ${qfile}`
+  
+    if [[ ! -z ${qloc} ]]; then
+      echo "Found object ${hash} in ${qloc}. Attempting to save..."
+      ./jake.sh ${qloc}
+    fi
+  done
+done
