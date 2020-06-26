ac4c36a54d310b087db27dfb37cee81dc7e27004
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 16:12:38 +1000

    add Glenn's suggestion to not do the same file

--- save_object.sh
+++ save_object.sh
@@ -4,9 +4,12 @@
 URL="https://swift.rc.nectar.org.au/v1/AUTH_1/swift-audit/audit.log"
 
 for hash in $(curl -s $URL); do
-  echo "Looking for ${hash}"
 
   for qfile in $(ls /root/swift-quarantine-*.txt); do
+    if [[ ! -f ${qfile} ]]; then
+      break
+    fi
+    echo "Looking for ${hash}"
     qloc=`grep ${hash} ${qfile}`
   
     if [[ ! -z ${qloc} ]]; then
