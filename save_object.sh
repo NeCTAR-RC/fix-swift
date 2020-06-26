725950baa355a4f02aadf70ec402c80b0cf2981e
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 16:30:35 +1000

    logic fixup

--- save_object.sh
+++ save_object.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 # save_object.sh
-
 URL="https://swift.rc.nectar.org.au/v1/AUTH_1/swift-audit/audit.log"
-
 for hash in $(curl -s $URL); do
-
   for qfile in /root/swift-quarantine-*.txt; do
     qloc=`grep ${hash} ${qfile}`
-    if [[ ! -f ${qloc} ]]; then
-      break
+    if [[ $? = 0 && ! -f "${qloc}" ]]; then
+      continue
     fi
     echo "Looking for ${hash}"
- 
-    if [[ ! -z "${qloc}" ]]; then
-      echo "Found object ${hash} in ${qloc}. Attempting to save..."
-      ./jake.sh ${qloc}
-    fi
+    echo "Found object ${hash} in ${qloc}. Attempting to save..."
+    echo ./jake.sh ${qloc}
   done
 done
