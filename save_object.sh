be148bc013446d63d46fd2a5a7b9392029fb5217
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 17:04:52 +1000

    fix grep

--- save_object.sh
+++ save_object.sh
@@ -3,7 +3,7 @@
 URL="https://swift.rc.nectar.org.au/v1/AUTH_1/swift-audit/audit.log"
 for hash in $(curl -s $URL); do
   for qfile in /root/swift-quarantine-*.txt; do
-    qloc=`grep ${hash} ${qfile}`
+    qloc=`grep -m 1 "/${hash}/" ${qfile}`
     if [[ $? != 0 && ! -f "${qloc}" ]]; then
       continue
     fi
