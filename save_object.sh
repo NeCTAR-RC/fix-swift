b2c1f1263903ae3387bf4e086531aa302474fb3c
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 16:31:51 +1000

    logic!

--- save_object.sh
+++ save_object.sh
@@ -4,7 +4,7 @@ URL="https://swift.rc.nectar.org.au/v1/AUTH_1/swift-audit/audit.log"
 for hash in $(curl -s $URL); do
   for qfile in /root/swift-quarantine-*.txt; do
     qloc=`grep ${hash} ${qfile}`
-    if [[ $? = 0 && ! -f "${qloc}" ]]; then
+    if [[ $? != 0 && ! -f "${qloc}" ]]; then
       continue
     fi
     echo "Looking for ${hash}"
