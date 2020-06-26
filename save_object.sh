cb903992698d0495166e1fd98051b705aae6e712
Jake Yip <jake.yip@unimelb.edu.au>
2020-06-26 16:16:49 +1000

    Stricter bash variable handling

--- save_object.sh
+++ save_object.sh
@@ -5,14 +5,14 @@ URL="https://swift.rc.nectar.org.au/v1/AUTH_1/swift-audit/audit.log"
 
 for hash in $(curl -s $URL); do
 
-  for qfile in $(ls /root/swift-quarantine-*.txt); do
+  for qfile in /root/swift-quarantine-*.txt; do
     if [[ ! -f ${qfile} ]]; then
       break
     fi
     echo "Looking for ${hash}"
     qloc=`grep ${hash} ${qfile}`
   
-    if [[ ! -z ${qloc} ]]; then
+    if [[ ! -z "${qloc}" ]]; then
       echo "Found object ${hash} in ${qloc}. Attempting to save..."
       ./jake.sh ${qloc}
     fi
